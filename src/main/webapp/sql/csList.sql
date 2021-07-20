a42b287f0d6c51532adc5e05282689993199e3bc
mijung-park <mezzang.park@gmail.com>
2021-07-21 04:20:29 +0900

    20210721 cslist/reply/member sql

--- src/main/webapp/sql/csList.sql
+++ src/main/webapp/sql/csList.sql
@@ -31,5 +31,50 @@ CREATE TABLE tbl_reply (
 
 DESC tbl_reply;
 
-CREATE TABLE tbl_user (
-	private
+CREATE TABLE tbl_member(
+  member_id  VARCHAR(50) PRIMARY KEY, -- 아이디
+  member_pw VARCHAR(100) NOT NULL, -- 비밀번호
+  member_name VARCHAR(30) NOT NULL, -- 이름
+  member_nickName VARCHAR(30) NOT NULL, -- 닉네임
+  member_birth DATE NOT NULL, -- 생일
+  member_phoneNum VARCHAR(30) NOT NULL, -- 폰번호
+  member_mail VARCHAR(100) NOT NULL, -- 이메일
+  member_addNum VARCHAR(100) NOT NULL, -- 우편번호
+  member_addCity VARCHAR(100) NOT NULL, -- 주소
+  regDate TIMESTAMP DEFAULT NOW(), -- 가입날짜
+  enabled TINYINT(1) DEFAULT 1
+);
+
+ALTER TABLE tbl_member Add member_grade VARCHAR(30);
+
+SELECT * FROM tbl_member;
+
+   	SELECT c.cs_seq cs_seq,
+   		   c.cs_category cs_category,
+   		   c.cs_title cs_title,
+   		   c.cs_writer cs_writer,
+   		   c.cs_readcnt cs_readcnt,
+   		   c.cs_content cs_content,
+   		   c.cs_secret cs_secret,
+   		   c.cs_status cs_status,
+   		   c.cs_filename cs_filename,
+   		   c.cs_replycnt cs_replycnt,
+   		   c.cs_regdate cs_regdate,
+   		   c.cs_updatedate cs_updatedate
+   	FROM tbl_cslist c LEFT JOIN tbl_reply r ON c.cs_seq = r.reply_seq
+   						   JOIN tbl_member m ON c.cs_writer = m.member_id
+	  	GROUP BY c.cs_seq
+   	ORDER BY c.cs_seq DESC;
+    
+    INSERT INTO tbl_cslist (
+      cs_seq, cs_category, cs_title, cs_content, cs_writer, cs_secret, cs_regdate, cs_filename) 
+      VALUES ((SELECT * FROM (SELECT MAX(cs_seq)+1 FROM tbl_cslist) next), 
+      		  3, 2, 43, 2, 3, NOW(), 3);
+              
+SELECT * FROM tbl_cslist;
+
+   UPDATE tbl_cslist SET 
+   		cs_replycnt = cs_replycnt + 6
+	WHERE cs_seq = 2;
+
+
