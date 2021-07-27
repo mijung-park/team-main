88f38be94ae5ecdfc10ca0c511283c5dc278dbf0
mijung-park <mezzang.park@gmail.com>
2021-07-27 17:29:58 +0900

    네브바 & 메인페이지 완료

--- src/main/webapp/WEB-INF/sql/category.sql
+++ src/main/webapp/WEB-INF/sql/category.sql
@@ -1,9 +1,19 @@
+desc tbl_category;
+
+select * from tbl_category;
+
+insert into tbl_category (category_main, category_sub)
+values ('키워드추천', '진단키트');
+
+DROP TABLE tbl_category;
+
 CREATE TABLE tbl_category (
-	category_seq INT(11) primary KEY AUTO_INCREMENT,
-    category_main VARCHAR(200) NOT NULL,
-    category_sub VARCHAR(200)
+	category_seq INT(11) primary key auto_increment,
+    category_main varchar(50) NOT NULL,
+    category_sub varchar(50) NOT NULL
 );
 
-DESC tbl_category;
+insert INTO tbl_category (category_main, category_sub)
+values ('자가진단키트', '진단키트');
 
-SELECT * FROM tbl_category;
+select * FROM tbl_category;
