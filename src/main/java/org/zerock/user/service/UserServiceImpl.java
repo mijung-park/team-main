d92cc1ca078717f14e2e6a801f5f762ec25e8c1a
mijung-park <mezzang.park@gmail.com>
2021-08-23 14:10:03 +0900

    문자API 수정

--- src/main/java/org/zerock/user/service/UserServiceImpl.java
+++ src/main/java/org/zerock/user/service/UserServiceImpl.java
@@ -81,7 +81,7 @@ public class UserServiceImpl implements UserService {
     // 4 params(to, from, type, text) are mandatory. must be filled
     HashMap<String, String> params = new HashMap<String, String>();
     params.put("to", user_phone);    // 수신전화번호
-    params.put("from", "010-6463-1824");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+    params.put("from", "010-8807-6027");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
     params.put("type", "SMS");
     params.put("text", "인증번호는" + "["+cerNum+"]" + "입니다.");
     params.put("app_version", "test app 1.2"); // application name and version
@@ -103,7 +103,7 @@ public class UserServiceImpl implements UserService {
 		
     HashMap<String, String> params = new HashMap<String, String>();
     params.put("to", user_phone);    // 수신전화번호
-    params.put("from", "010-6463-1824");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+    params.put("from", "010-8807-6027");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
     params.put("type", "LMS");
     params.put("text", smsContent);
     params.put("app_version", "test app 1.2"); // application name and version
